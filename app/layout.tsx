7f86ba081b2d68bad71a0f3d359b71a0e67a9acc
Eduarda Godoz <eduarda.godoz@gmail.com>
2025-02-26 20:57:52 -0300

    teste

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ['latin'],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}
+      >
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
