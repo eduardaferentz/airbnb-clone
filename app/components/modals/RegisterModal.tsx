c590808dac42eb12186e85d54fbbfe0a37e162c1
Eduarda Godoz <eduarda.godoz@gmail.com>
2025-03-03 20:56:21 -0300

    salvar

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,10 @@ import { FcGoogle } from 'react-icons/fc'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import useRegisterModal from '@/app/hooks/useRegisterModal'
+import Modal from './Modal'
+import Heading from '../Heading'
+import Input from '../inputs/Input'
+import toast from 'react-hot-toast'
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal()
@@ -33,12 +37,58 @@ const RegisterModal = () => {
                 registerModal.onClose()
             })
             .catch((error) => {
-                console.log(error)
+                toast.error('Something went wrong')
+            })
+            .finally(() => {
+                setIsLoading(false)
             })
     }
 
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading
+                title='Welcome to Airbnb'
+                subtitle='Create an account!'>
+            </Heading>
+            <Input 
+                id='email'
+                label='Email'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required>
+            </Input>
+            <Input 
+                id='name'
+                label='Name'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required>
+            </Input>
+            <Input 
+                id='password'
+                type='password'
+                label='Passwordx'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required>
+            </Input>
+        </div>
+    )
+
     return ( 
-        <div></div>
+        <Modal 
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title="Register"
+            actionLabel="Continue"
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
+        >
+        </Modal>
     )
 }
 
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,13 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import RegisterModal from "../modals/RegisterModal";
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal()
     const [isOpen, setIsOpen] = useState(false)
+    
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value)
     }, [])
@@ -34,7 +38,7 @@ const UserMenu = () => {
                     <div className="flex flex-col cursor-pointer">
                         <>
                             <MenuItem onClick={() => {}} label="Login"></MenuItem>
-                            <MenuItem onClick={() => {}} label="Sign up"></MenuItem>
+                            <MenuItem onClick={registerModal.onOpen} label="Sign up"></MenuItem>
 
                         </>
                     </div>
